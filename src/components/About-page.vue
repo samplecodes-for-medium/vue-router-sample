9716a4a1015fa2fda47ec2c9c6d939a477db1ea4
samplecodes-for-medium <58945886+samplecodes-for-medium@users.noreply.github.com>
2020-01-15 00:53:25 +0300

    vue-router-sample
    vue-router-sample

---
+++ src/components/About-page.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <img width="25%" src="../assets/logo.png">
+    <h1>About Page</h1>
+    <h2>It is me, Ercan...</h2>
+    <h3>Router:</h3>
+    <a @click="routeToHomePage">Go to Home</a>
+  </div>
+</template>
+<script>
+import router from "../routes";
+export default {
+  name: "About-page",
+  methods: {
+    routeToHomePage() {
+      router.push("home");
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Home-page.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img width="25%" src="../assets/logo.png">
+    <h1>Home Page</h1>
+    <h2>Welcome to Home Page</h2>
+    <h3>Router:</h3>
+    <router-link to="/about">Go to About</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home-page"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
